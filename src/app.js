19412af7be7851bdf7ae5c7c03903b8a73d8b0f1
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-23 21:37:27 +0000

    Added some simple styling using SCSS and webpack plugins.

--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
+import "./styles/styles.scss"
 
 ReactDOM.render(<IndecisionApp options={["Thing one", "Thing two", "Thing three"]}/>, document.getElementById("app"));
