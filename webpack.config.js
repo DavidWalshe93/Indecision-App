694f8f3924eedbcbaba9a836c3591a5e6d38297d
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-23 19:55:29 +0000

    Added webpack to project to handle compiles of react apps. Installed all babel dependencies required to render React Apps.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// Created by David Walshe on 23/02/2020
+
+// Import built-in modules
+const path = require("path");
+
+// Export webpack setup
+module.exports = {
+    mode: "development",
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+};
