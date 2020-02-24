13baaec1e58fd2910a0ed7428c8905d979e63d27
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-24 01:36:28 +0000

    Added styling for modal views

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -14,10 +14,16 @@ export default class OptionModal extends React.Component {
             isOpen={!!this.props.selectedOption}
             onRequestClose={this.props.handleCloseModal}
             contentLabel="Selected Option"
+            closeTimeoutMS={200}
+            className={"modal"}
         >
-            <h3>Selected Option</h3>
-            {this.props.selectedOption && <p>{this.props.selectedOption}</p>}
-            <button onClick={this.props.handleCloseModal}>Okay</button>
+            <h3 className={"modal__title"}>Selected Option</h3>
+            {this.props.selectedOption && <p className={"modal__body"}>{this.props.selectedOption}</p>}
+            <button
+                onClick={this.props.handleCloseModal}
+                className={"button"}
+            >Okay
+            </button>
         </Modal>
     );
 }
